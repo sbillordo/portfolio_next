b4d7864b7e7c14c616d634d4be1bf4ede0db1a33
sbillordo <billordo.santiago@gmail.com>
2025-07-15 21:42:04 +0200

    favicon en lauout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Santiago Billordo',
   description: 'Mi portfolio personal',
+   icons: {
+    icon: '/favicon.ico',        // ruta absoluta → nunca se le añade /es, /en…
+    shortcut: '/favicon.ico',
+    apple: '/apple-touch-icon.png'
+  }
 };
 
 export default function RootLayout({

107be4e7362c8fa803a47c74c062c00413331294
sbillordo <billordo.santiago@gmail.com>
2025-07-16 13:36:22 +0200

    se corrige el favicon finalemnte con facivon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,6 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Santiago Billordo',
   description: 'Mi portfolio personal',
-   icons: {
-    icon: '/favicon.ico',        // ruta absoluta → nunca se le añade /es, /en…
-    shortcut: '/favicon.ico',
-    apple: '/apple-touch-icon.png'
-  }
 };
 
 export default function RootLayout({
